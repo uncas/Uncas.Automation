834db61788a92b81d25ecd23e268753e262336d5
Ole Lynge Soerensen <olelynge@gmail.com>
2024-01-03 17:42:04 +0100

    Clean up.

--- Source/Tools/Ai/QaPipeline.py
+++ Source/Tools/Ai/QaPipeline.py
@@ -4,7 +4,6 @@
 # https://docs.haystack.deepset.ai/docs/ready_made_pipelines#extractiveqapipeline
 
 import os
-import ssl
 import logging
 from haystack.document_stores import InMemoryDocumentStore
 from haystack.pipelines.standard_pipelines import TextIndexingPipeline
@@ -15,16 +14,7 @@ from haystack.pipelines import ExtractiveQAPipeline
 def questionDocuments(documentDirectory, question):
     logging.basicConfig(format="%(levelname)s - %(name)s -  %(message)s", level=logging.WARNING)
     logging.getLogger("haystack").setLevel(logging.WARN)
-
     document_store = InMemoryDocumentStore(use_bm25=True)
-
-    try:
-        _create_unverified_https_context = ssl._create_unverified_context
-    except AttributeError:
-        pass
-    else:
-        ssl._create_default_https_context = _create_unverified_https_context
-
     files_to_index = [documentDirectory + "/" + f for f in os.listdir(documentDirectory)]
     indexing_pipeline = TextIndexingPipeline(document_store)
     indexing_pipeline.run_batch(file_paths=files_to_index)
--- Source/Tools/Ai/QaPipelineTests.py
+++ Source/Tools/Ai/QaPipelineTests.py
@@ -1,7 +1,17 @@
+import ssl
 from QaPipeline import questionDocuments
 from haystack.utils import fetch_archive_from_http
 
 documentDirectory = "Data/GameOfThrones"
+
+try:
+	_create_unverified_https_context = ssl._create_unverified_context
+except AttributeError:
+	pass
+else:
+	ssl._create_default_https_context = _create_unverified_https_context
+
+
 url = "https://s3.eu-central-1.amazonaws.com/deepset.ai-farm-qa/datasets/documents/wiki_gameofthrones_txt1.zip"
 fetch_archive_from_http(url = url, output_dir = documentDirectory)
 question = "What's the name of Cersei's children?"

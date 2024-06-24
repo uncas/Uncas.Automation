82e9203dad07c42aeefcb340da9388064da86aff
Ole Lynge Soerensen <olelynge@gmail.com>
2024-06-24 17:09:24 +0200

    Implement answer pipeline.

--- Simple/ScrimbaAiEng/UserFlow.py
+++ Simple/ScrimbaAiEng/UserFlow.py
@@ -8,8 +8,18 @@ def askQuestion(question):
 	standaloneQuestionTemplate = 'Given a question, convert it to a standalone question. Question: {question}. Standalone question:'
 	standaloneQuestionPrompt = PromptTemplate.from_template(standaloneQuestionTemplate)
 	chain = standaloneQuestionPrompt.pipe(llm).pipe(StrOutputParser()).pipe(vectorStoreRetriever)
-	response = chain.invoke({ "question": question })
-	return response
+	docs = chain.invoke({ "question": question })
+	answerTemplate = "Given a question and a context, provide an answer to the user. " + \
+ 		"We want this answer to be friendly, " + \
+ 		"only answer from the context provided and never make up answers, " + \
+ 		"apologise if you do not know the answer and advise the " + \
+ 		"user to email help@scrimba.com. Question: {question}. Context: {context}. Answer: "
+	answerPrompt = PromptTemplate.from_template(answerTemplate)
+	answerChain = answerPrompt.pipe(llm)
+	context = docs[0].page_content
+	print(" *** Context: ", context)
+	answer = answerChain.invoke({ "question": question, "context": context })
+	return answer.content
 
 response = askQuestion('What are the technical requirements for running Scrimba? I only have a very old laptop which is not that powerful.')
 print(response)

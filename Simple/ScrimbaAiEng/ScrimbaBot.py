d72261237c078b42efeb7fde3d17fc949bf4a676
Ole Lynge Soerensen <olelynge@gmail.com>
2024-06-24 12:53:07 +0200

    Add embedding from Scrimba course.

---
+++ Simple/ScrimbaAiEng/ScrimbaBot.py
@@ -0,0 +1,33 @@
+# From Scrimba course in AI Engineering
+
+# import
+from langchain_chroma import Chroma
+from langchain_community.document_loaders import TextLoader
+from langchain_community.embeddings.sentence_transformer import (
+    SentenceTransformerEmbeddings,
+)
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+# load the document and split it into chunks
+loader = TextLoader("scrimba-info.txt")
+documents = loader.load()
+
+# split it into chunks
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=50)
+docs = text_splitter.split_documents(documents)
+
+# create the open-source embedding function
+embedding_function = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
+
+# query it
+query = "How can I use Scrimba?"
+
+# save to disk
+db2 = Chroma.from_documents(docs, embedding_function, persist_directory="./chroma_db")
+docs = db2.similarity_search(query)
+print(docs[0].page_content)
+
+# load from disk
+db3 = Chroma(persist_directory="./chroma_db", embedding_function=embedding_function)
+docs = db3.similarity_search(query)
+print(docs[0].page_content)

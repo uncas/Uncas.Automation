37c70841d82566513c94f8b62ff0a5382c702f30
Ole Lynge Soerensen <olelynge@gmail.com>
2024-07-03 10:20:18 +0200

    Refactor.

--- Simple/PersonalAssistant/Functions/theMovieDb.py
+++ Simple/PersonalAssistant/Functions/theMovieDb.py
@@ -1,27 +1,31 @@
-# https://www.themoviedb.org/movie/now-playing
+# Example of what this service can do: https://www.themoviedb.org/movie/now-playing
 
 # Docs: https://developer.themoviedb.org/docs/getting-started
 # Forum: https://www.themoviedb.org/talk/category/5047958519c29526b50017d6
 # Wrappers and libraries: https://developer.themoviedb.org/docs/wrappers-and-libraries
 # Status: https://status.themoviedb.org/
 
-# Lib: https://github.com/celiao/tmdbsimple/?tab=readme-ov-file
+# The python lib that I'm using: https://github.com/celiao/tmdbsimple/?tab=readme-ov-file
 
 # API Keys: https://www.themoviedb.org/settings/api
 
-def getWatchProviders(movieInfo):
-	movieTitle = movieInfo["movieTitle"]
-	myCountry = "DK"
-	myProviders = ["Viaplay", "HBO Max", "Netflix", "TV 2 Play"]
-	import tmdbsimple as tmdb
-	import os
-	tmdb.API_KEY = os.getenv('THEMOVIEDB_API_KEY')
+import tmdbsimple as tmdb
+import os
+tmdb.API_KEY = os.getenv('THEMOVIEDB_API_KEY')
+
+def getBestMatchingMovieId(movieTitle):
 	search = tmdb.Search()
 	response = search.movie(query=movieTitle)
 	movieResponse = response["results"][0]
-	movieId = movieResponse["id"]
+	return movieResponse["id"]
+
+def getWatchProviders(movieInfo):
+	movieTitle = movieInfo["movieTitle"]
+	movieId = getBestMatchingMovieId(movieTitle)
 	movie = tmdb.Movies(movieId)
 	watchProviders = movie.watch_providers()
+	myCountry = "DK"
+	myProviders = ["Viaplay", "HBO Max", "Netflix", "TV 2 Play"]
 	providersInMyCountry = watchProviders["results"][myCountry]
 	types = ["rent", "buy", "flatrate", "ads"]
 	options = []

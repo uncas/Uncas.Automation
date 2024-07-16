72d3d7540af27dd3a00bfce837b04822ff880819
Ole Lynge Soerensen <olelynge@gmail.com>
2024-07-16 23:27:04 +0200

    Improve the logging.

--- Source/Flows/PersonalAssistant/LoggerSetup.py
+++ Source/Flows/PersonalAssistant/LoggerSetup.py
@@ -1,3 +1,14 @@
 def initLogger():
 	import logging
-	logging.basicConfig(filename='Data/Log.log', level=logging.INFO)
+	# Formats: https://stackoverflow.com/a/16759818
+	#defaultFormat = "%(levelname)s:%(name)s:%(message)s"
+	advancedFormat = "[%(asctime)s] %(levelname)s [%(name)s.%(funcName)s:%(lineno)d] %(message)s"
+	logging.basicConfig(
+		level = logging.INFO,
+		format = advancedFormat,
+		datefmt = "%Y-%m-%d %H:%M:%S",
+		handlers = [
+			logging.StreamHandler(),
+			logging.FileHandler("Data/Log.log", "a"),
+		]
+	)

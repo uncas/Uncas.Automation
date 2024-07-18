f4bf954262f9e3026d4c329a8d776d34dca35290
Ole Lynge Soerensen <olelynge@gmail.com>
2024-07-18 08:06:02 +0200

    Refactor shell scripts.

--- test.sh
+++ test.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-if [ ! -d ".venv" ]; then
-  echo "Creating virtual environment .venv for python."
-  python3 -m venv .venv
-fi
+./init.sh
 
-source .venv/bin/activate
-python -m pip install --upgrade pip -q
-pip install -r requirements.txt -q
-#python Source/Test.py
 python -m unittest discover -s Source -p *UnitTests.py

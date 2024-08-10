f07998d73f33a9ddbd74951ce9c751f70b7eeea8
Ole Lynge Soerensen <olelynge@gmail.com>
2024-08-10 09:00:23 +0200

    Use file util from lib.

--- tests/test_GoogleDriveService_adhoc.py
+++ tests/test_GoogleDriveService_adhoc.py
@@ -2,6 +2,6 @@ def test_exportDocumentAsMarkdown():
 	from uncas_automation.Services.Google.GoogleDriveService import exportDocumentAsMarkdown
 	id = "blabla"
 	result = exportDocumentAsMarkdown(id)
-	from uncas_automation.Utils.FileUtils import write_text
+	from easai.utils.file_utils import write_text
 	print(result[:2000])
 	write_text("Output", "doc.md", result)

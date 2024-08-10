94c586b076982cf99bd13e8155c8ac6c87175211
Ole Lynge Soerensen <olelynge@gmail.com>
2024-08-10 19:53:18 +0200

    Use new lib version.

--- uncas_automation/assistant/personal_assistant.py
+++ uncas_automation/assistant/personal_assistant.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 from openai import OpenAI
 
 from easai.assistant.log import SqliteAiLog
-from easai.assistant.loop import run_tool_loop, get_system_prompt, get_user_prompt
+from easai.assistant.loop import run_assistant_loop, get_user_prompt, AssistantLoop
 from easai.assistant.chat_console import run_chat_console, print_assistant_message, print_tool_message
 
 from uncas_automation.assistant.assistant_tools import get_all_tools
@@ -42,14 +42,23 @@ def get_llm_model() -> str:
 	return "sam4096/qwen2tools:1.5b"
 
 def run_tasked_agent(agent: AgentDefinition):
-	client = get_llm_client()
-	messages = [get_system_prompt(agent.system_prompt)]
 	user_message_content = ""
 	for inputTask in agent.input_tasks:
 		taskResult = json.dumps(inputTask["task"]())
 		user_message_content += inputTask["prompt"] + ": " + taskResult + "\n\n"
-	messages.append(get_user_prompt(user_message_content))
-	messages = run_tool_loop_here(client, agent.tools, messages)
+	messages = [get_user_prompt(user_message_content)]
+	loop = AssistantLoop(
+		client = get_llm_client(), 
+		model = get_llm_model(),
+		system_prompt = agent.system_prompt,
+		tools = agent.tools,
+		max_iterations = 10)
+	messages = run_assistant_loop(
+		messages = messages,
+		assistant_loop = loop,
+		assistant_message_callback = print_assistant_message,
+		tool_message_callback = print_tool_message,
+		ai_logger = ai_log)
 	assistantMessage = messages[-1].content
 	agent.action_on_result(assistantMessage)
 
@@ -58,28 +67,19 @@ def run_chat_loop():
 		logger = logging.getLogger(__name__)
 		logger.critical('FATAL ERROR: OPENAI_API_KEY needed. Set the value in a .env file: echo "OPENAI_API_KEY=YOUR_API_KEY_VALUE" >> .env')
 		exit(1)
-	run_chat_console(
+	assistant_loop = AssistantLoop(
 		client = get_llm_client(), 
 		model = get_llm_model(),
 		system_prompt = read_system_prompt_file("InteractiveAssistantLoop.md"),
+		tools = get_all_tools())
+	run_chat_console(
+		assistant_loop = assistant_loop,
 		your_name = getSetting("assistant", {}).get("callName", "You"),
-		tools = get_all_tools(),
 		ai_logger = ai_log)
 
 def read_system_prompt_file(file_name):
 	with open("uncas_automation/assistant/Prompts/" + file_name, "r") as file:
 		return file.read()
-
-def run_tool_loop_here(client: OpenAI, tools: list, messages: list):
-	return run_tool_loop(
-		client,
-		tools,
-		messages,
-		model = get_llm_model(),
-		max_iterations = 10,
-		assistant_message_callback = print_assistant_message,
-		tool_message_callback = print_tool_message,
-		ai_logger = ai_log)
 
 def run_personal_assistant():
 	init_logger()
